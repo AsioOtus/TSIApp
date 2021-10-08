4548d8b7b7f9f5ba5eff1e592c8ab668aac6849c
Asio Otus <asio.otus.verus@icloud.com>
2021-10-08 15:52:56 +0300

    TSIAPP-2 | Fix unselected filters message alignment

--- TSIApp/Source/Views/Schedule/Schedule.Table/PeriodView.swift
+++ TSIApp/Source/Views/Schedule/Schedule.Table/PeriodView.swift
@@ -8,41 +8,29 @@ extension Schedule.Table {
 		@State var leadingColumnWidth: CGFloat?
 		
 		var body: some View {
-			EmptyView()
-			if vm.appState.group == .empty && vm.appState.lecturer == .empty && vm.appState.room == .empty {
-				VStack(alignment: .center) {
-					Text(Local[.noFilterSelected])
-						.padding()
-					Text("\(Local[.filterSetInstructionStart]) \"\(Image(systemName: "slider.horizontal.3"))\" \(Local[.filterSetInstructionEnd])")
-						.font(.footnote)
-						.padding()
-						.multilineTextAlignment(.center)
+			switch vm.days {
+			case .notInitialized:
+				Text("Refresh")
+				
+			case .loading:
+				List {
+					EmptyDayView2(startDate: vm.period.startDate, endDate: vm.period.endDate)
 				}
-			} else {
-				switch vm.days {
-				case .notInitialized:
-					Text("Refresh")
-
-				case .loading:
-					List {
-						EmptyDayView2(startDate: vm.period.startDate, endDate: vm.period.endDate)
+				.environment(\.defaultMinListRowHeight, 10)
+				.listStyle(InsetGroupedListStyle())
+				
+			case .loaded(let days):
+				List {
+					ForEach(days, id: \.date) { day in
+						Schedule.Table.DayView(day: day)
+							.environment(\.leadingColumnWidth, leadingColumnWidth)
 					}
-					.environment(\.defaultMinListRowHeight, 10)
-					.listStyle(InsetGroupedListStyle())
-
-				case .loaded(let days):
-					List {
-						ForEach(days, id: \.date) { day in
-							Schedule.Table.DayView(day: day)
-								.environment(\.leadingColumnWidth, leadingColumnWidth)
-						}
-					}
-					.environment(\.defaultMinListRowHeight, 10)
-					.listStyle(InsetGroupedListStyle())
-
-				case .failed(let error):
-					Text(String(describing: error))
 				}
+				.environment(\.defaultMinListRowHeight, 10)
+				.listStyle(InsetGroupedListStyle())
+				
+			case .failed(let error):
+				Text(String(describing: error))
 			}
 		}
 	}
--- TSIApp/Source/Views/Schedule/Schedule.Table/TableView.swift
+++ TSIApp/Source/Views/Schedule/Schedule.Table/TableView.swift
@@ -35,17 +35,21 @@ extension Schedule.Table {
 					.padding()
 					.background(App.colorScheme.main.color.edgesIgnoringSafeArea([.leading, .trailing]))
 					
-					Pager(page: vm.page, data: vm.periodViewModels, id: \.period) { periodVM in
-						Schedule.Table.PeriodView(vm: periodVM)
+					if vm.appState.group == .empty && vm.appState.lecturer == .empty && vm.appState.room == .empty {
+						unchoosenFilterErrorMessageView
+					} else {
+						Pager(page: vm.page, data: vm.periodViewModels, id: \.period) { periodVM in
+							Schedule.Table.PeriodView(vm: periodVM)
+						}
+						.draggingAnimation(.interactive)
+						.onPageChanged { i in
+							withAnimation {	vm.pageChanged(i) }
+						}
+						.padding(0)
+						.edgesIgnoringSafeArea(.all)
 					}
-					.draggingAnimation(.standard(duration: 0.5))
-					.onPageChanged { i in
-						withAnimation {	vm.pageChanged(i) }
-					}
-					.padding(0)
-					.edgesIgnoringSafeArea(.all)
 				}
-				.navigationBarTitle(navigationBarTitleText, displayMode: .inline)
+				.navigationBarTitleDisplayMode(.inline)
 				.navigationBarItems(leading: scheduleSettingsButton, trailing: todayButton)
 				.toolbar {
 					ToolbarItem(placement: .principal) {
@@ -62,125 +66,63 @@ extension Schedule.Table {
 				Schedule.SettingsView(vm: .init(appState: appState), isShown: $isPresentingSettingsView)
 			}
 		}
-		
-		var todayButton: some View {
-			Button(action: {
-				isPresentingDateView = true
-			}) {
-				Image.calendar.font(.system(size: 24))
-			}
-			.foregroundColor(.white)
-		}
-		
-		var scheduleSettingsButton: some View {
-			Button(action: {
-				self.isPresentingSettingsView = true
-			}) {
-				Image.sliders.font(.system(size: 24))
-			}
-			.foregroundColor(.white)
-		}
-		
-		var title: some View {
-			VStack {
-				HStack {
-					if !vm.appState.group.value.isEmpty {
-						Text(vm.appState.group.value)
-							.font(.headline.weight(.bold))
-					}
-					
-					if !vm.appState.group.value.isEmpty && !vm.appState.room.value.isEmpty {
-						Text("|")
-							.foregroundColor(.gray)
-					}
-					
-					if !vm.appState.room.value.isEmpty {
-						Text(vm.appState.room.value)
-							.font(.headline.weight(.bold))
-					}
-				}
-				
-				if vm.appState.lecturer != .empty {
-					Text(vm.appState.lecturer.value)
-				}
-			}
-			.foregroundColor(.white)
-		}
 	}
 }
 
 extension Schedule.Table.TableView {
-	struct TitleView: View {
-		let groupName: String
-		let roomIndex: String
-		
-		var body: some View	{
+	var todayButton: some View {
+		Button(action: {
+			isPresentingDateView = true
+		}) {
+			Image.calendar.font(.system(size: 24))
+		}
+		.foregroundColor(.white)
+	}
+	
+	var scheduleSettingsButton: some View {
+		Button(action: {
+			self.isPresentingSettingsView = true
+		}) {
+			Image.sliders.font(.system(size: 24))
+		}
+		.foregroundColor(.white)
+	}
+	
+	var title: some View {
+		VStack {
 			HStack {
-				if !groupName.isEmpty {
-					Text(groupName)
+				if !vm.appState.group.value.isEmpty {
+					Text(vm.appState.group.value)
+						.font(.headline.weight(.bold))
 				}
 				
-				if !roomIndex.isEmpty {
-					Text(roomIndex)
-						.bold()
+				if !vm.appState.group.value.isEmpty && !vm.appState.room.value.isEmpty {
+					Text("|")
+						.foregroundColor(.gray)
+				}
+				
+				if !vm.appState.room.value.isEmpty {
+					Text(vm.appState.room.value)
+						.font(.headline.weight(.bold))
 				}
 			}
-		}
-	}
-	
-	var navigationBarTitleText: String {
-		var text = [String]()
-		
-		if !vm.appState.group.value.isEmpty {
-			text.append(vm.appState.group.value)
-		}
-		
-		if !vm.appState.room.value.isEmpty {
-			text.append(vm.appState.room.value)
-		}
-		
-		return text.joined(separator: " – ")
-	}
-}
-
-struct NavigationBarModifier: ViewModifier {
-	
-	var backgroundColor: UIColor?
-	
-	init ( backgroundColor: UIColor?) {
-		self.backgroundColor = backgroundColor
-		let coloredAppearance = UINavigationBarAppearance()
-		coloredAppearance.configureWithTransparentBackground()
-		coloredAppearance.backgroundColor = .clear
-		coloredAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
-		coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
-		
-		UINavigationBar.appearance().standardAppearance = coloredAppearance
-		UINavigationBar.appearance().compactAppearance = coloredAppearance
-		UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
-		UINavigationBar.appearance().tintColor = .white
-		
-	}
-	
-	func body(content: Content) -> some View {
-		ZStack{
-			content
-			VStack {
-				GeometryReader { geometry in
-					Color(self.backgroundColor ?? .clear)
-						.frame(height: geometry.safeAreaInsets.top)
-						.edgesIgnoringSafeArea(.top)
-					Spacer()
-				}
+			
+			if vm.appState.lecturer != .empty {
+				Text(vm.appState.lecturer.value)
 			}
 		}
-	}
-}
-
-extension View {
-	
-	func navigationBarColor(_ backgroundColor: UIColor?) -> some View {
-		self.modifier(NavigationBarModifier(backgroundColor: backgroundColor))
+		.foregroundColor(.white)
 	}
 	
+	var unchoosenFilterErrorMessageView: some View {
+		VStack(alignment: .center) {
+			Text(Local[.noFilterSelected])
+				.padding()
+			Text("\(Local[.filterSetInstructionStart]) \"\(Image(systemName: "slider.horizontal.3"))\" \(Local[.filterSetInstructionEnd])")
+				.font(.footnote)
+				.padding()
+				.multilineTextAlignment(.center)
+		}
+		.frame(maxHeight: .infinity)
+	}
 }
